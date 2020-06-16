ffd8de2e41623db397add7bff54a1cacb04604c8
George <gwilliams@nekls.org>
2020-06-16 17:21:50 -0500

    Update conf

--- source/conf.py
+++ source/conf.py
@@ -94,6 +94,12 @@ html_logo = 'next.grey.small.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    'css_files': [
+        '_static/override.css',  # overrides for wide tables in RTD theme
+        ],
+    }
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
